e026211db51e92f8052c222f989f1834a0805e66
Giovanna Scalco <giovannabarrosscalco@gmail.com>
2025-10-07 09:44:50 -0300

    Implementando a Criação de Anotações

--- notes-frontend/src/App.jsx
+++ notes-frontend/src/App.jsx
@@ -2,26 +2,35 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Note from "./components/Note";
 import AppBar from "./components/AppBar";
+import Formulario from "./components/Formulario";
 import "./App.css";
 
 function App() {
   const [notes, setNotes] = useState([]); 
 
-  useEffect(() => {
+  const carregaNotas = () => {
     axios
       .get("http://localhost:8000/api/notes/")
       .then((res) => setNotes(res.data));
+  }
+
+    useEffect(() => {
+    carregaNotas();
   }, []);
 
-  console.log(notes);
-
-    return (
+return (
     <>
       <AppBar />
       <main className="container">
+        <Formulario loadNotes={carregaNotas} />
         <div className="card-container">
           {notes.map((note) => (
-            <Note key={`note__${note.id}`} title={note.title}>
+            <Note
+              key={`note__${note.id}`}
+              id={note.id}
+              title={note.title}
+              loadNotes={carregaNotas}
+            >
               {note.content}
             </Note>
           ))}
--- notes-frontend/src/components/Note/index.jsx
+++ notes-frontend/src/components/Note/index.jsx
@@ -1,10 +1,26 @@
+import axios from "axios";
 import "./index.css";
 
 export default function Note(props) {
+  const { id, title, children, loadNotes } = props;
+
+  const deleteNote = () => {
+    axios
+      .delete(`http://localhost:8000/api/notes/${id}/`)
+      .then(() => {
+        loadNotes();
+      })
+      .catch((error) => console.error("Erro ao deletar nota:", error));
+  };
+
   return (
     <div className="card">
-      <h3 className="card-title">{props.title}</h3>
-      <div className="card-content">{props.children}</div> 
+      <h3 className="card-title">{title}</h3>
+      <div className="card-content">{children}</div>
+
+      <button className="btn-delete" onClick={deleteNote}>
+        🗑️ Deletar
+      </button>
     </div>
   );
 }
